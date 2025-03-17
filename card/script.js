d3bcf0fdabcf9921ab755d8d832cb6934868ba9d
Mon-Amor <moniquespamorim@gmail.com>
2025-03-17 12:48:30 -0300

    ajuste10

--- card/script.js
+++ card/script.js
@@ -89,3 +89,14 @@ function gerarCardsDeEventos(eventos) {
 
 // Chamar a função para gerar os cards de eventos
 gerarCardsDeEventos(eventos);
+// Função para trocar o tema
+function changeTheme(theme) {
+    document.body.className = theme;
+    localStorage.setItem('theme', theme);
+}
+
+// Carregar o tema salvo no localStorage
+window.onload = function () {
+    const savedTheme = localStorage.getItem('theme') || 'theme-light';
+    document.body.className = savedTheme;
+};
